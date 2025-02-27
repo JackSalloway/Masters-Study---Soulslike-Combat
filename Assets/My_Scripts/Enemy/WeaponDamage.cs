8ff758e291dbb2840d19fbd3e79bd29c3ab0801d
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-02-27 09:15:59 +0000

    check if player is parrying

--- Assets/My_Scripts/Enemy/WeaponDamage.cs
+++ Assets/My_Scripts/Enemy/WeaponDamage.cs
@@ -4,6 +4,8 @@ public class WeaponDamage : MonoBehaviour
 {
     public float damage; // Variable used to store the damage value of the weapon
 
+    [SerializeField] private EnemyStats enemy; // Variable assigned to the EnemyStats script component
+
     // OnTirggerEnter triggers when the object this code is assigned to collides with another collider
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +16,14 @@ public class WeaponDamage : MonoBehaviour
 
             // Assign new playerMovement variable to the PlayerMovement script component on the player game object
             PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+
+            // Check if the player is parrying
+            if (player.animator.GetBool("isParrying") == true) {
+                enemy.poise -= 1000; // Deal a very large amount of poise damage to the enemy
+                enemy.poiseBroken = true; // Set poiseBroken variable to true to start the timer for resetting poise
+                enemy.animator.SetBool("stanceBroken", true); // Trigger the stance broken animation
+                return; // Early return to prevent any more code being ran
+            };
 
             // Check if the players iframes are currently active. If false: deal damage to the player. If true: print a message to the console
             if (playerMovement.invulnerabilityFramesActive == false) player.DamagePlayer(damage); // Make the player take damage!
