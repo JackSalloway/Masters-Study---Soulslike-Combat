516550d8c78355a40fb54489ac10ffbe65a6e3bd
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-02-28 12:29:33 +0000

    implement text reset functionality

--- Assets/My_Scripts/Player/TextAlert.cs
+++ Assets/My_Scripts/Player/TextAlert.cs
@@ -6,10 +6,15 @@ public class TextAlert : MonoBehaviour
     public TextMeshPro alertText; // Variable assigned to the TMP object above the player character mesh.
     
     private string actionType; // String variable used to discern if the action was a dodge or a parry
+    private float textTimer = 0; // Variable used to reset the text value of the TMP object after 3 seconds
 
     void Update()
     {
         alertText.transform.forward = Camera.main.transform.forward; // Make the text rotate with the camera
+
+        if (alertText.text != "") textTimer += Time.deltaTime; // Start the timer if the text value is not an empty string
+        
+        if (textTimer >= 3) ResetText(); // When the timer is greater than or equal to 3 seconds, call the ResetText method        
     }
 
     // Method for setting the text value of the TMP object to "!" when the player performs a successful dodge or parry 
@@ -17,5 +22,12 @@ public class TextAlert : MonoBehaviour
     {
         alertText.SetText("!"); // Set the text above the player characters head
         actionType = action; // Set the actionType variable to the action performed by the player
+    }
+
+    // Method for reseting variable values to their original values
+    private void ResetText() {
+        alertText.SetText(""); // Set the text value to an empty string
+        actionType = null; // Set the action type to null
+        textTimer = 0;
     }
 }
