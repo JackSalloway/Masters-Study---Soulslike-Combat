2b32562ca75ac763b68a68490f7e9db8bba17dfa
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-02-28 10:06:47 +0000

    change variable type

--- Assets/My_Scripts/Player/TextAlert.cs
+++ Assets/My_Scripts/Player/TextAlert.cs
@@ -3,8 +3,7 @@ using TMPro;
 
 public class TextAlert : MonoBehaviour
 {
-
-    public TextMeshProUGUI alertText; // Variable assigned to the TMP object above the player character mesh.
+    public TextMeshPro alertText; // Variable assigned to the TMP object above the player character mesh.
 
     // Method for setting the text value of the TMP object to the provided string
     // String examples: "", "Parried!", "Dodged!" 
