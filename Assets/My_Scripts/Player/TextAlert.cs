6a6f01524bcc2b28586060a9aed5e857248d9269
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-02-28 10:46:25 +0000

    refactor TextAlert script logic

--- Assets/My_Scripts/Player/TextAlert.cs
+++ Assets/My_Scripts/Player/TextAlert.cs
@@ -4,11 +4,13 @@ using TMPro;
 public class TextAlert : MonoBehaviour
 {
     public TextMeshPro alertText; // Variable assigned to the TMP object above the player character mesh.
+    
+    private string actionType; // String variable used to discern if the action was a dodge or a parry
 
-    // Method for setting the text value of the TMP object to the provided string
-    // String examples: "", "Parried!", "Dodged!" 
-    public void SetText(string text)
+    // Method for setting the text value of the TMP object to "!" when the player performs a successful dodge or parry 
+    public void AlertPlayer(string action)
     {
-        alertText.SetText(text);
+        alertText.SetText("!"); // Set the text above the player characters head
+        actionType = action; // Set the actionType variable to the action performed by the player
     }
 }
