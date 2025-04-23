69551a900d152268cc3354ff85abbfcc749f2989
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-04-23 12:40:22 +0100

    handle mouse input

--- Assets/My_Scripts/Player/PlayerInputs.cs
+++ Assets/My_Scripts/Player/PlayerInputs.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class PlayerInputs : MonoBehaviour
 {
+    [Header("Mouse Input Values")]
+    private float mouseX; // Variable to store mouse X axis movement
+    private float mouseY; // Variable to store mouse Y axis movement
+
     [Header("Tutorial Values")]
     [SerializeField] private float tabHoldDuration; // Variable used to store how long the player needs to hold the TAB key to toggle tutorials
     [SerializeField] private float tabHoldTimer; // Variable used to track how long the TAB key is held for
 
     [Header("Script References")]
     [SerializeField] private TuitionData tutorialController; // Reference to the TuitionData script
+    [SerializeField] private PlayerCamera playerCamera; // Reference to the PlayerCamera script
 
     // Update is called once per frame
     void Update()
@@ -30,5 +35,11 @@ public class PlayerInputs : MonoBehaviour
         
         // Check if the player releases the TAB key and reset the tabHoldTimer variable to 0 when they do
         if (Input.GetKeyUp(KeyCode.Tab)) tabHoldTimer = 0;
+
+        // ------------------------------------------------
+        // MOUSE INPUT - Handles player camera manipulation
+        mouseX = Input.GetAxis("Mouse X"); // Get X axis input
+        mouseY = Input.GetAxis("Mouse Y"); // Get Y axis input
+        playerCamera.SetMouseInput(new Vector2(mouseX, mouseY)); // Call SetMouseInput method in PlayerCamera script
     }
 }
