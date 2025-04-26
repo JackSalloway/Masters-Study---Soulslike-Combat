2a4171b04992f1ed11eb43d51076f59446325205
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-04-26 20:08:13 +0100

    Update comments

--- Assets/My_Scripts/Interfaces/IInteractable.cs
+++ Assets/My_Scripts/Interfaces/IInteractable.cs
@@ -1,5 +1,5 @@
 public interface IInteractable
 {
-    string InteractionPrompt { get;} // All objects using the interaction interface must use 
-    void Interact(); // Defines a method that all interactable objects must have
+    string InteractionPrompt { get;} // Returns the verb related to the action (example: open, pick up, pull)
+    void Interact(); // Method to interact with the object (example: open door, pick up item)
 }
