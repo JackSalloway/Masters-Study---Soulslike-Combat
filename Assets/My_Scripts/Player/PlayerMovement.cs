f1576dd63499db8c2b10c80eb580c6beb988f971
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-04-26 21:02:33 +0100

    update comments

--- Assets/My_Scripts/Player/PlayerMovement.cs
+++ Assets/My_Scripts/Player/PlayerMovement.cs
@@ -18,9 +18,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb; // Variable assigned to the player characters rigid body component
 
     [Header("Script References")]
-    [SerializeField] private PlayerStamina playerStats; // Variable assigned to PlayerStamina script. Will be assigned in the inspection window
-    [SerializeField] private PlayerHealth playerHealth; // Variable assigned to the PlayerHealth script. Assigned in the inspection window.
-    [SerializeField] private PlayerCamera playerCamera; // Variable assigned to the Player Camera script. Assigned in the inspection window
+    [SerializeField] private PlayerStamina playerStats; // Reference to the PlayerStamina script.
+    [SerializeField] private PlayerHealth playerHealth; // Reference to the the PlayerHealth script.
+    [SerializeField] private PlayerCamera playerCamera; // Reference to the the Player Camera script.
 
     void Awake()
     {
