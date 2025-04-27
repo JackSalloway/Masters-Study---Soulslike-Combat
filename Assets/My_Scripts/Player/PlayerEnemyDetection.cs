24e400591d401679ccd7df6ec13bebc39704b684
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-04-27 15:50:48 +0100

    re-add DetectEnemies method

--- Assets/My_Scripts/Player/PlayerEnemyDetection.cs
+++ Assets/My_Scripts/Player/PlayerEnemyDetection.cs
@@ -15,6 +15,27 @@ public class PlayerEnemyDetection : MonoBehaviour
         CheckEnemies();
     }
 
+    // Method to detect enemies within the detectionRadius variable
+    public void DetectEnemies()
+    {
+        // Reset the value of the currentlyDetectedEnemies list each time the method is called
+        currentlyDetectedEnemies.Clear();
+
+        // Create an array of colliders within the detection radius range around the player
+        Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius);
+
+        // Loop over each colliders found 
+        foreach(var collider in colliders)
+        {   
+            // Check for the "Enemy" tag on the collider
+            if (collider.CompareTag("Enemy"))
+            {   
+                // Add the enemy game object to the current list
+                currentlyDetectedEnemies.Add(collider.gameObject);          
+            };
+        }
+    }
+
     // Method to check if enemies are still present
     public void CheckEnemies()
     {
