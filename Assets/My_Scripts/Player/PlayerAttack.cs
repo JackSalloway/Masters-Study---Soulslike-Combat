fc1b7989b96fc48febe3b61867d8af86fe5ea56b
JackSalloway <52927105+JackSalloway@users.noreply.github.com>
2025-04-29 17:25:44 +0100

    Make typing cost stamina

--- Assets/My_Scripts/Player/PlayerAttack.cs
+++ Assets/My_Scripts/Player/PlayerAttack.cs
@@ -9,12 +9,14 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private TMP_InputField inputField; // Variable assigned to the input field game object in the canvas
     [SerializeField] private PlayerMovement playerMovement; // Variable assigned to the PlayerMovement script component
     [SerializeField] private EnemyStats enemy; // Variable assigned to the EnemyStats script component
+    [SerializeField] private float staminaDrainRate; // Variable used to drain stamina while the typing interface is active
     private float damage; // Variable used to set the amount of damage the comment will deal
 
     [Header("Script References")]
     [SerializeField] private PlayerInputs playerInputs; // Reference to the PlayerInputs script
     [SerializeField] private PlayerAnimationController playerAnimController; // Reference to the PlayerAnimationController script
     [SerializeField] private PlayerHealth playerHealth; // Reference the the PlayerHealthScript
+    [SerializeField] private PlayerStamina playerStamina; // Reference the the PlayerStamina Script
 
     void Start()
     {
@@ -26,10 +28,15 @@ public class PlayerAttack : MonoBehaviour
         // Deactivate the input field if the player dies (cleanup)
         if (playerHealth.playerIsDead)
         {   
-            inputField.DeactivateInputField();
-            inputField.gameObject.SetActive(false); // Disable the input field
+            CloseInterface();
             return; // prevent any further code being ran
         }
+
+        // Slowly drain stamina while the player is typing (attacking)
+        if (isTyping) playerStamina.SpendStamina(staminaDrainRate);
+
+        // If Player runs out of stamina, reset the interface for the next attack
+        if (playerStamina.stamina <= 0) ResetValues();
     }
 
     // Method to start the attack process. Triggered by left clicking after a successful roll or parry
@@ -48,12 +55,21 @@ public class PlayerAttack : MonoBehaviour
     public void EndVerbalAttack()
     {
         enemy.TakeDamage(damage, 0); // Deal damage to the enemy and 0 poise damage
-        inputField.gameObject.SetActive(false); // Disable the input field
+        ResetValues();
+    }
+
+    // Method to reset variable values for next attack
+    private void ResetValues()
+    {
         playerMovement.allowInput = true; // Enable player movement inputs
         isTyping = false; // set isTyping to false to allow the player to attack again after
         damage = 0; // Set the damage back to 0 ready for the next attack
         inputField.text = ""; // Set the input field text value to an empty string for the next attack
         playerInputs.preventInputs = false; // Re-enable all player inputs found in the PlayerInputs script
+        CloseInterface(); // Call CloseInterface Method
     }
+
+    // Method to clean up variable values when the typing interface closes
+    private void CloseInterface() => inputField.gameObject.SetActive(false); // Disable the input field
 }
 
